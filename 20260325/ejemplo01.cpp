efe9946009c04a2b96655fc014fa8a42972f1403
Angel <43154124+angel-utn@users.noreply.github.com>
2026-03-25 19:43:38 -0300

    Add files via upload

---
+++ 20260325/ejemplo01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1, num2, resultado;
+
+    cout << "Ingresar dos números: ";
+    cin >> num1;
+    cin >> num2;
+    resultado = num1 + num2;
+    cout << "El resultado de la suma es: ";
+    cout << resultado;
+
+    return 0;
+}
