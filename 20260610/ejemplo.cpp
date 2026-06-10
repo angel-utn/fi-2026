42a238dba8cc16b6dd410951c0d4ee1baba50d39
Angel <43154124+angel-utn@users.noreply.github.com>
2026-06-10 19:20:27 -0300

    Add files via upload

--- 20260610/ejemplo.cpp
+++ 20260610/ejemplo.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+  int i, k, numeroMaquina, cantidad, tiempo;
+  int totalPiezas;
+
+  for(k=1; k<=3; k++){
+    totalPiezas = 0;
+
+    for(i=1; i<=5; i++){
+      cout << "Número de maquina: ";
+      cin >> numeroMaquina;
+
+      cout << "Cantidad de piezas: ";
+      cin >> cantidad;
+
+      cout << "Tiempo: ";
+      cin >> tiempo;
+
+      totalPiezas += cantidad;
+    }
+    cout << endl;
+    cout << "Número de máquina: " << numeroMaquina << endl;
+    cout << "Total de piezas: " << totalPiezas << endl;
+  }
+  return 0;
+}
